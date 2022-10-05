88e7a2e690b3dbf9e4c59220f000095ea7be8ea6
Pavel <Pavel@DESKTOP-DOB86IK>
2022-10-05 19:10:49 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace assignment4_PavelGolovan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numberLabel = new System.Windows.Forms.Label();
+            this.numberTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelRadioButton = new System.Windows.Forms.RadioButton();
+            this.messageBoxRadioButton = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.resultLabel1 = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.resultLabel2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // numberLabel
+            // 
+            this.numberLabel.AutoSize = true;
+            this.numberLabel.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numberLabel.Location = new System.Drawing.Point(12, 9);
+            this.numberLabel.Name = "numberLabel";
+            this.numberLabel.Size = new System.Drawing.Size(456, 19);
+            this.numberLabel.TabIndex = 0;
+            this.numberLabel.Text = "Enter a nonnegative integer to calculate the factorial of that number:\r\n";
+            // 
+            // numberTextBox
+            // 
+            this.numberTextBox.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numberTextBox.Location = new System.Drawing.Point(269, 40);
+            this.numberTextBox.Name = "numberTextBox";
+            this.numberTextBox.Size = new System.Drawing.Size(191, 27);
+            this.numberTextBox.TabIndex = 1;
+            this.numberTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Display the result in a";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(234, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 19);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "or in a";
+            // 
+            // labelRadioButton
+            // 
+            this.labelRadioButton.AutoSize = true;
+            this.labelRadioButton.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelRadioButton.Location = new System.Drawing.Point(169, 76);
+            this.labelRadioButton.Name = "labelRadioButton";
+            this.labelRadioButton.Size = new System.Drawing.Size(59, 23);
+            this.labelRadioButton.TabIndex = 4;
+            this.labelRadioButton.TabStop = true;
+            this.labelRadioButton.Text = "label";
+            this.labelRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // messageBoxRadioButton
+            // 
+            this.messageBoxRadioButton.AutoSize = true;
+            this.messageBoxRadioButton.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.messageBoxRadioButton.Location = new System.Drawing.Point(290, 76);
+            this.messageBoxRadioButton.Name = "messageBoxRadioButton";
+            this.messageBoxRadioButton.Size = new System.Drawing.Size(112, 23);
+            this.messageBoxRadioButton.TabIndex = 5;
+            this.messageBoxRadioButton.TabStop = true;
+            this.messageBoxRadioButton.Text = "message box";
+            this.messageBoxRadioButton.UseVisualStyleBackColor = true;
+            this.messageBoxRadioButton.CheckedChanged += new System.EventHandler(this.messageBoxRadioButton_CheckedChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(408, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 19);
+            this.label4.TabIndex = 6;
+            this.label4.Text = ".";
+            // 
+            // resultLabel1
+            // 
+            this.resultLabel1.AutoSize = true;
+            this.resultLabel1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resultLabel1.Location = new System.Drawing.Point(265, 111);
+            this.resultLabel1.Name = "resultLabel1";
+            this.resultLabel1.Size = new System.Drawing.Size(195, 19);
+            this.resultLabel1.TabIndex = 8;
+            this.resultLabel1.Text = "The factorial of that number:";
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.AutoSize = true;
+            this.calculateButton.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.calculateButton.Location = new System.Drawing.Point(364, 183);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(81, 29);
+            this.calculateButton.TabIndex = 9;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.clearButton.Location = new System.Drawing.Point(273, 183);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(85, 29);
+            this.clearButton.TabIndex = 10;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.exitButton.Location = new System.Drawing.Point(623, 228);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(85, 29);
+            this.exitButton.TabIndex = 11;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // resultLabel2
+            // 
+            this.resultLabel2.BackColor = System.Drawing.SystemColors.Window;
+            this.resultLabel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.resultLabel2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resultLabel2.Location = new System.Drawing.Point(207, 142);
+            this.resultLabel2.Name = "resultLabel2";
+            this.resultLabel2.Size = new System.Drawing.Size(316, 27);
+            this.resultLabel2.TabIndex = 12;
+            this.resultLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(720, 269);
+            this.Controls.Add(this.resultLabel2);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.resultLabel1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.messageBoxRadioButton);
+            this.Controls.Add(this.labelRadioButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numberTextBox);
+            this.Controls.Add(this.numberLabel);
+            this.Name = "Form1";
+            this.Text = "Calculating the Factorial of a Number";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label numberLabel;
+        private System.Windows.Forms.TextBox numberTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton labelRadioButton;
+        private System.Windows.Forms.RadioButton messageBoxRadioButton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label resultLabel1;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label resultLabel2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace assignment4_PavelGolovan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void messageBoxRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            resultLabel2.Text = "";
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            sbyte n, count;
+            long result;
+            count = 1;
+            result = count;
+
+            if (sbyte.TryParse(numberTextBox.Text, out n))
+            {
+                if (n < 0)
+                {
+                    MessageBox.Show("Please enter a nonnegative integer value.");
+                }
+                else if (n == 0)
+                {
+                    if (labelRadioButton.Checked == true)
+                    {
+                        resultLabel2.Text = "1";
+                    }
+                    else
+                    {
+                        MessageBox.Show("The factorial of the number 0 is 1.");
+                    }
+                }
+                else if (n > 20)
+                {
+                    MessageBox.Show("Sorry, the number is too large for program to handle. Try something smaller.");
+                }
+                else
+                {
+                    while (count <= n)
+                    {
+                        result *= count;
+                        count++;
+                    }
+                if (labelRadioButton.Checked == true)
+                    {
+                        resultLabel2.Text = result.ToString();
+                    }
+                else
+                    {
+                        MessageBox.Show("The factorial of the number " + n.ToString() + " is " + result.ToString() + ".");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid input. Please enter a nonnegative integer value.");
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            numberTextBox.Text = "";
+            resultLabel2.Text = "";
+            numberTextBox.Focus();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
